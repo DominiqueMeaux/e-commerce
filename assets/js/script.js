65360439b94789dcc66e375de922c5122cac9a0e
dominiquem <dominique.meaux@gmail.com>
2018-07-06 08:38:14 +0200

    mise en page

--- assets/js/script.js
+++ assets/js/script.js
@@ -72,3 +72,49 @@ $(function(){
   });
 });
 //Fin script Amélie
+var acc = document.getElementsByClassName("accordion");
+var i;
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+var acc = document.getElementsByClassName("accordion2");
+var i;
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+};
+$('#hptxt1').click(function(){
+  $('#accessories').hide();
+  $('.accessories').hide();
+  $('.woodPool').show();
+  $('#woodPool').show();
+  $('#accordionPool').show();
+});
+$('#hptxt2').click(function(){
+  $('.woodPool').hide();
+  $('#woodPool').hide();
+  $('#accordionPool').hide();
+  $('#accessories').show();
+  $('.accessories').show();
+});
+//  FONCTION RETOUR EN HAUT DE PAGE
+$(function(){
+$('#hbutton').click(function(){
+  $("html, body").animate({scrollTop: 0},"slow");
+});
+});
